c43e8f4487253a729fd96a174b81d8c3c2dd903f
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-07-31 17:17:58 +0900

    Add Field entity

---
+++ src/Entity/Field.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Field
+{
+    /**
+     * @var string the type of the field
+     */
+    private $type;
+
+    /**
+     * @var string the name of the field
+     */
+    private $name;
+
+    /**
+     * Field constructor.
+     *
+     * @param string $type the type of the field
+     * @param string $name the name of the field
+     */
+    public function __construct(string $type, string $name)
+    {
+        $this->type = $type;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string the type of the field
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type the type of the field
+     *
+     * @return self
+     */
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return string the name of the field
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name the name of the field
+     *
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
